c9f38eb241c0449e5a3b92322d9264d321fad7ba
pattarapol <firstpatma@gmail.com>
2025-03-04 09:41:16 +0700

    add more

--- main.tf
+++ main.tf
@@ -9,13 +9,11 @@ terraform {
 
 provider "docker" {}
 
-# Pull Node.js image
 resource "docker_image" "node_app" {
   name         = "node:16"  # ใช้ Node.js version 16
   keep_locally = false
 }
 
-# Create container
 resource "docker_container" "web_app" {
   image = docker_image.node_app.name
   name  = "web-app-container"
@@ -25,15 +23,12 @@ resource "docker_container" "web_app" {
     external = 3000
   }
 
-  # Mount volume เพื่อ copy code เข้าไปใน container
   volumes {
     host_path      = "${path.cwd}/"  # path ปัจจุบันที่มี source code
     container_path = "/app"
   }
 
-  # คำสั่งเริ่มต้น container
   command = ["npm", "start"]
 
-  # Working directory
   working_dir = "/app"
 }
